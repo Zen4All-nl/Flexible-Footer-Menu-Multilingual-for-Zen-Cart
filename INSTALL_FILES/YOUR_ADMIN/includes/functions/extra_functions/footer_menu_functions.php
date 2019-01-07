8ed136d6ff0c4277e1616663460b3a7177d24bf4
Zen4All <info@zen4all.nl>
2019-01-07 22:33:13 +0100

    update code formatting

--- INSTALL_FILES/YOUR_ADMIN/includes/functions/extra_functions/footer_menu_functions.php
+++ INSTALL_FILES/YOUR_ADMIN/includes/functions/extra_functions/footer_menu_functions.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  *
  * Flexible Footer Menu Multilingual
@@ -9,20 +10,19 @@
  *
  * @added for version 1.0 by ZCAdditions.com (rbarbour) 4-17-2013 $
  *
-**/
-
+ * */
 if (!defined('IS_ADMIN_FLAG')) {
-    die('Illegal Access');
+  die('Illegal Access');
 }
 
-function zen_set_flexible_footer_menu_status($page_id, $status) {
-global $db;
-    if ($status == '1') {
-      return $db->Execute("update " . TABLE_FLEXIBLE_FOOTER_MENU . " set status = '1' where page_id = '" . $page_id . "'");
-    } elseif ($status == '0') {
-      return $db->Execute("update " . TABLE_FLEXIBLE_FOOTER_MENU . " set status = '0' where page_id = '" . $page_id . "'");
-    } else {
-      return -1;
-    }
+function zen_set_flexible_footer_menu_status($page_id, $status)
+{
+  global $db;
+  if ($status == '1') {
+    return $db->Execute("UPDATE " . TABLE_FLEXIBLE_FOOTER_MENU . " SET status = 1 WHERE page_id = " . (int)$page_id);
+  } elseif ($status == '0') {
+    return $db->Execute("UPDATE " . TABLE_FLEXIBLE_FOOTER_MENU . " SET status = 0 WHERE page_id = " . (int)$page_id);
+  } else {
+    return -1;
   }
-?>
+}
